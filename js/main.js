b2aa29fb2970566556be37baa8c7cc8f7b78a124
Tomasz Kusmierczyk <tomasz.t.kusmierczyk@gmail.com>
2021-12-14 17:13:13 +0100

    wersja pierwsza

---
+++ js/main.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+
+    $('.accordion-item').click(function() {
+        
+        if ($(this).hasClass('open')) {
+            $(this).removeClass('open');
+            $(this).find('.accordion-content').slideUp();
+
+        } else {
+            $(this).addClass('open');
+            $(this).find('.accordion-content').slideDown();
+        }
+
+    });
+    
+
+});
